017dc26cb0771459cd778e84a3f6e8e082f35cf7
Anshu <mushu.aradhyula@gmail.com>
2017-12-24 02:25:25 -0600

    Commented algorithm and included css reset

---
+++ sketch.js
@@ -0,0 +1,26 @@
+//make_square() returns one grid square
+let make_square = () => {
+    //create a div
+    let div = document.createElement('div');
+    //apply the grid square css class
+    div.className = ".square";
+    //return this div
+};
+
+//make_grid(int n) creates a grid of squares of size n * n, n E [2,16]
+let make_grid = (int n) => {
+  //calculate the width and height each grid would have to be.
+  //calculate the number of rows and columns needed.
+  //create flex-box divs according to number of rows and columns.
+  //create n*n squares and append child to the flex-box divs.
+};
+
+//reset() resets the color of the grid
+let reset = () => {
+  //create new grid
+}
+
+//change_color() changes the set color upon click
+let change_color = () => {
+
+}
